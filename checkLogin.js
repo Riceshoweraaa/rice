c40f98f78f4fdae76cd9e8ea00da40bb2ff7123f
Riceshoweraaa <3062820453@qq.com>
2025-01-16 14:29:42 +0800

    Update checkLogin.js

--- checkLogin.js
+++ checkLogin.js
@@ -2,15 +2,15 @@
 function checkLogin() {
     var isLoggedIn = Cookies.get('isLoggedIn');
     if (isLoggedIn !== 'true') {
-        window.location.href = 'login.html'; // 如果未登录，重定向到登录页面
+        window.location.href = 'login.html'; 
     } else {
-        // 已登录，可以加载courseware页面的内容
+        
         loadCoursewareContent();
     }
 }
 
 function loadCoursewareContent() {
-    // 在这里加载courseware页面的内容
+    
     console.log('Loading courseware content for logged in user.');
-    // 你可以在这里添加代码来动态加载课程内容
+    
 }
